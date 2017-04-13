6f459d38363da7c97ff69af95162eb172a0ed431
xbearx1987 <xbearx1987@gmail.com>
2017-04-13 16:10:41 +0800

    add validates/message

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,14 @@
 class Job < ApplicationRecord
+  validates :name, presence: { message: "请填写职称" }
+  validates :content, presence: { message: "请填写职位描述" }
+  validates :company, presence: { message: "请填写公司名称" }
+  validates :category, presence: { message: "请选择职位类型" }
+  validates :location, presence: { message: "请填写工作地点" }
+  validates :wage_lower_bound, presence: { message: "请填写最低薪水" }
+  validates :wage_upper_bound, presence: { message: "请填写最高薪水" }
+  validates :wage_lower_bound, numericality: { less_than: :wage_upper_bound, message: "薪水下限不能高于薪水上限" }
+  validates :wage_upper_bound, numericality: { greater_than: 0, message: "最小薪水必须大于零" }
+  validates :contact_mail, presence: { message: "请填写联系邮箱" }
+
   belongs_to :user
 end
