08a6bd33d02a915178f8f6dfada1a44fce0ab3a7
xbearx1987 <xbearx1987@gmail.com>
2017-04-13 16:19:14 +0800

    setup job edit/update/destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_job_and_check_permission , only: [:edit, :update, :destroy]
 
   def index
     @jobs = Job.all
@@ -24,7 +25,33 @@ class JobsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @job.update(job_params)
+
+    if @job.save
+      redirect_to jobs_path, notice: '职缺修改成功。'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job.destroy
+      redirect_to jobs_path, alert: '职缺删除成功。'
+  end
+
 private
+
+  def find_job_and_check_permission
+    @job = Job.find(params[:id])
+
+    if @job.user != current_user
+      redirect_to root_path, alert: "你没有权限进行此操作。"
+    end
+  end
 
   def job_params
     params.require(:job).permit(:name,:content,:company,:category,:location,:wage_lower_bound,:wage_upper_bound,:contact_mail)
