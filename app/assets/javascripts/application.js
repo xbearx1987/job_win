42353a0ebc80eeba09fa751225632a0c9446f9c6
xbearx1987 <xbearx1987@gmail.com>
2017-04-16 01:17:52 +0800

    add bootstrap/carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,34 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap/alert
-//= require bootstrap/dropdown 
+//= require bootstrap/dropdown
+//= require bootstrap/carousel
 //= require_tree .
-
-$(document).ready(function() {
-    //sponsors
-    $(".sponsors").owlCarousel({
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
-      items : 4,
-      nav : true,
-      transitionStyle:"fade",
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
-
-    });
-
-    ///smooth scroll
-    $(function() {
-      $('.scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html, body').animate({
-              scrollTop: target.offset().top - 69
-            }, 800);
-            return false;
-          }
-        }
-      });
-    });
-});
--- app/controllers/collections_controller.rb
+++
@@ -1,8 +0,0 @@
-class CollectionsController < ApplicationController
-
-  def index
-    @user = current_user
-    @collections = Collection.where(:user => @user).order("created_at DESC")
-  end
-
-end
