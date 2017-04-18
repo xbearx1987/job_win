84bd69a950dd1fed1f0245cc165e0c98b40a07ce
xbearx1987 <xbearx1987@gmail.com>
2017-04-18 10:32:56 +0800

    update collections page

--- app/controllers/account/collections_controller.rb
+++ app/controllers/account/collections_controller.rb
@@ -2,7 +2,8 @@ class Account::CollectionsController < ApplicationController
 
   def index
     @user = current_user
-    @collections = Collection.where(:user => @user).order("created_at DESC")
+    @collections = Collection.where(:user => @user).order("created_at DESC").paginate(:page => params[:page], :per_page => 7)
+    @suggests = Job.where(:is_hidden => false).limit(5).order("RANDOM()")
   end
 
 end
