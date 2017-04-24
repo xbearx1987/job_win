3ada538380cf55dceeb67022e8b7ac842c40f73e
xbearx1987 <xbearx1987@gmail.com>
2017-04-24 15:58:04 +0800

    add model location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424075354) do
+ActiveRecord::Schema.define(version: 20170424075730) do
 
   create_table "collections", force: :cascade do |t|
     t.integer  "user_id"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 20170424075354) do
     t.datetime "updated_at",                       null: false
     t.integer  "user_id"
     t.boolean  "is_hidden",        default: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "resumes", force: :cascade do |t|
