e81f8b6bc088aec50c4a2f471e3dbc9fc022fa40
xbearx1987 <xbearx1987@gmail.com>
2017-04-24 16:06:32 +0800

    setup admin/location

---
+++ app/controllers/admin/locations_controller.rb
@@ -0,0 +1,6 @@
+class Admin::LocationsController < ApplicationController
+
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
+    resources :locations
+
     resources :jobs do
       member do
         post :publish
