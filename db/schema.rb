0feb94f588a354d23951b2f12c2fae25463b4caf
xbearx1987 <xbearx1987@gmail.com>
2017-04-25 13:33:12 +0800

    new model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424082055) do
+ActiveRecord::Schema.define(version: 20170425053157) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "is_hidden",  default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "collections", force: :cascade do |t|
     t.integer  "user_id"
